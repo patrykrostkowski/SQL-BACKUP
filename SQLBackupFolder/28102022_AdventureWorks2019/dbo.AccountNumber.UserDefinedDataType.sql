5f0352509191e6cb74caa0174a0a92847c7056fe
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-10-28 15:00:03 +0200

    delte qwe

--- SQLBackupFolder/28102022_AdventureWorks2019/dbo.AccountNumber.UserDefinedDataType.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  UserDefinedDataType [dbo].[AccountNumber]    Script Date: 28.10.2022 14:57:41 ******/
-CREATE TYPE [dbo].[AccountNumber] FROM [nvarchar](15) NULL
-GO
