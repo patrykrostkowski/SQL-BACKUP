45339e92ec107af0bb0aa77d5f0719f1ffa95398
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-10-28 12:38:02 +0200

    ADV DELETE

--- SQLBackupFolder/28102022_AdventureWorks2019/dbo.AccountNumber.UserDefinedDataType.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  UserDefinedDataType [dbo].[AccountNumber]    Script Date: 28.10.2022 12:23:30 ******/
-CREATE TYPE [dbo].[AccountNumber] FROM [nvarchar](15) NULL
-GO
--- SQLBackupFolder/28102022_AdventureWorks2019/dbo.Phone.UserDefinedDataType.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  UserDefinedDataType [dbo].[Phone]    Script Date: 28.10.2022 12:23:30 ******/
-CREATE TYPE [dbo].[Phone] FROM [nvarchar](25) NULL
-GO
