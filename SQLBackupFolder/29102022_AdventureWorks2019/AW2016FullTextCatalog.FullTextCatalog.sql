0826eada6e414192177b488d58e6d28a0d107163
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-10-29 12:25:02 +0200

    29.10.2022_AdventureWorks2019 backup

--- SQLBackupFolder/29102022_AdventureWorks2019/AW2016FullTextCatalog.FullTextCatalog.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  FullTextCatalog [AW2016FullTextCatalog]    Script Date: 29.10.2022 12:19:33 ******/
-CREATE FULLTEXT CATALOG [AW2016FullTextCatalog] AS DEFAULT
-GO
