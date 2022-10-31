b3853b68739a007fc133846858ef0a663131542c
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-10-31 11:42:47 +0100

    31.10.2022_CustomerWarehouse backup

---
+++ SQLBackupFolder/31102022_CustomerWarehouse/dbo.SQlprofilingdata.Table.sql
@@ -0,0 +1,21 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[SQlprofilingdata]    Script Date: 31.10.2022 11:42:46 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SQlprofilingdata](
+	[Name] [varchar](50) NULL,
+	[Email Adress] [varchar](50) NULL,
+	[D O B] [varchar](50) NULL,
+	[Salary] [varchar](50) NULL,
+	[CountryCode] [varchar](50) NULL,
+	[EMP Code] [varchar](50) NULL,
+	[Dept Code] [varchar](50) NULL,
+	[Country] [varchar](50) NULL,
+	[CountryTaxcode] [varchar](50) NULL,
+	[TaxPercentage] [varchar](50) NULL,
+	[Pan card] [varchar](50) NULL
+) ON [PRIMARY]
+GO
