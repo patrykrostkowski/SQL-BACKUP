32aed5b60e1796ecb87ddd1a50bb860b4f91d92a
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-10-31 12:25:34 +0100

    31.10.2022_Test backup

--- SQLBackupFolder/31102022_Test/dbo.tblFinalist.Table.sql
+++ SQLBackupFolder/31102022_Test/dbo.tblFinalist.Table.sql
@@ -1,6 +1,6 @@
 ﻿USE [Test]
 GO
-/****** Object:  Table [dbo].[tblFinalist]    Script Date: 31.10.2022 12:25:14 ******/
+/****** Object:  Table [dbo].[tblFinalist]    Script Date: 31.10.2022 12:25:32 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
