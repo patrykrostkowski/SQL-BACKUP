51db4278498164e9262010724149cc22f034cdf7
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-10-31 09:19:04 +0100

    31.10.2022_AdventureWorks2019 backup

---
+++ SQLBackupFolder/31102022_AdventureWorks2019/HumanResources.vEmployeeDepartment.View.sql
@@ -0,0 +1,32 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  View [HumanResources].[vEmployeeDepartment]    Script Date: 31.10.2022 09:19:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [HumanResources].[vEmployeeDepartment] 
+AS 
+SELECT 
+    e.[BusinessEntityID] 
+    ,p.[Title] 
+    ,p.[FirstName] 
+    ,p.[MiddleName] 
+    ,p.[LastName] 
+    ,p.[Suffix] 
+    ,e.[JobTitle]
+    ,d.[Name] AS [Department] 
+    ,d.[GroupName] 
+    ,edh.[StartDate] 
+FROM [HumanResources].[Employee] e
+	INNER JOIN [Person].[Person] p
+	ON p.[BusinessEntityID] = e.[BusinessEntityID]
+    INNER JOIN [HumanResources].[EmployeeDepartmentHistory] edh 
+    ON e.[BusinessEntityID] = edh.[BusinessEntityID] 
+    INNER JOIN [HumanResources].[Department] d 
+    ON edh.[DepartmentID] = d.[DepartmentID] 
+WHERE edh.EndDate IS NULL
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Returns employee name, title, and current department.' , @level0type=N'SCHEMA',@level0name=N'HumanResources', @level1type=N'VIEW',@level1name=N'vEmployeeDepartment'
+GO
