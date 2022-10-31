1993c048623a2675089f8ea34ab09bf2919bb816
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-10-31 12:01:18 +0100

    31.10.2022_CustomerWarehouse backup

---
+++ SQLBackupFolder/31102022_CustomerWarehouse/dbo.dimStates.Table.sql
@@ -0,0 +1,16 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[dimStates]    Script Date: 31.10.2022 12:01:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[dimStates](
+	[StatesID] [int] NOT NULL,
+	[StatesName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dimStates] PRIMARY KEY CLUSTERED 
+(
+	[StatesID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
