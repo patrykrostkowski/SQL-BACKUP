672fbba93652a9aeb139b810a7f74e5badcb1f8d
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-10-31 11:39:36 +0100

    31.10.2022_AdventureWorks2019 backup

---
+++ SQLBackupFolder/31102022_AdventureWorks2019/AW2016FullTextCatalog.FullTextCatalog.sql
@@ -0,0 +1,5 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  FullTextCatalog [AW2016FullTextCatalog]    Script Date: 31.10.2022 11:39:33 ******/
+CREATE FULLTEXT CATALOG [AW2016FullTextCatalog] AS DEFAULT
+GO
---
+++ SQLBackupFolder/31102022_AdventureWorks2019/dbo.Phone.UserDefinedDataType.sql
@@ -0,0 +1,5 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  UserDefinedDataType [dbo].[Phone]    Script Date: 31.10.2022 11:39:33 ******/
+CREATE TYPE [dbo].[Phone] FROM [nvarchar](25) NULL
+GO
