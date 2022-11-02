477b084ded3119acaa4ede73cf808a384355a457
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-02 12:46:07 +0100

    02.11.2022_CustomerWarehouse backup

--- SQLBackupFolder/02112022_CustomerWarehouse/dbo.dimCountry.Table.sql
+++ SQLBackupFolder/02112022_CustomerWarehouse/dbo.dimCountry.Table.sql
@@ -1,6 +1,6 @@
 ﻿USE [CustomerWarehouse]
 GO
-/****** Object:  Table [dbo].[dimCountry]    Script Date: 02.11.2022 12:45:19 ******/
+/****** Object:  Table [dbo].[dimCountry]    Script Date: 02.11.2022 12:46:05 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
