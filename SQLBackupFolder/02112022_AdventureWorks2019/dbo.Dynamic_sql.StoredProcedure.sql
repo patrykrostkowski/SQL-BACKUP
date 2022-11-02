9dab9b457798989b29186a56b09136862bb40f38
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-02 13:59:01 +0100

    02.11.2022_AdventureWorks2019 backup

---
+++ SQLBackupFolder/02112022_AdventureWorks2019/dbo.Dynamic_sql.StoredProcedure.sql
@@ -0,0 +1,30 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  StoredProcedure [dbo].[Dynamic_sql]    Script Date: 02.11.2022 13:58:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[Dynamic_sql] (@NameToSearch VARCHAR(10),@SearchPattern VARCHAR(30))
+AS
+BEGIN
+	DECLARE @DynamicSQL VARCHAR(MAX)
+	DECLARE @ColumnName VARCHAR(20)
+
+		IF LOWER(@NameToSearch) = 'first'
+			SET @ColumnName = 'FirstName'
+		IF LOWER(@NameToSearch) = 'middle'
+			SET @ColumnName = 'MiddleName'
+		IF LOWER(@NameToSearch) = 'last'
+			SET @ColumnName = 'LastName'	
+			
+	SET @DynamicSQL = 'SELECT *
+						FROM Person.Person pp
+						WHERE 
+							pp.' + @ColumnName
+	SET @DynamicSQL = @DynamicSQL + '	LIKE ''%' + @SearchPattern + '%'''
+
+EXEC (@DynamicSQL)
+	
+END
+GO
---
+++ SQLBackupFolder/02112022_AdventureWorks2019/dbo.NameStyle.UserDefinedDataType.sql
@@ -0,0 +1,5 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  UserDefinedDataType [dbo].[NameStyle]    Script Date: 02.11.2022 13:58:57 ******/
+CREATE TYPE [dbo].[NameStyle] FROM [bit] NOT NULL
+GO
