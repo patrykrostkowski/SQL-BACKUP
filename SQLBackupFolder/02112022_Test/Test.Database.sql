fb7742189fc95067cf147daca3f9db5dc0dc240c
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-02 09:21:24 +0100

    02.11.2022_Test backup

--- SQLBackupFolder/02112022_Test/Test.Database.sql
+++ SQLBackupFolder/02112022_Test/Test.Database.sql
@@ -1,6 +1,6 @@
 ﻿USE [master]
 GO
-/****** Object:  Database [Test]    Script Date: 02.11.2022 09:10:02 ******/
+/****** Object:  Database [Test]    Script Date: 02.11.2022 09:21:22 ******/
 CREATE DATABASE [Test]
  CONTAINMENT = NONE
  ON  PRIMARY 
--- SQLBackupFolder/02112022_Test/dbo.tblCategory.Table.sql
+++ SQLBackupFolder/02112022_Test/dbo.tblCategory.Table.sql
@@ -1,6 +1,6 @@
 ﻿USE [Test]
 GO
-/****** Object:  Table [dbo].[tblCategory]    Script Date: 02.11.2022 09:10:02 ******/
+/****** Object:  Table [dbo].[tblCategory]    Script Date: 02.11.2022 09:21:22 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
