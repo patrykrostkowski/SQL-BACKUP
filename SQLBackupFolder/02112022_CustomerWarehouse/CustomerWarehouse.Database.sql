b6aee77375cf532e16b12c6cd8f47bb832ad5489
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-02 14:17:20 +0100

    02.11.2022_CustomerWarehouse backup

--- SQLBackupFolder/02112022_CustomerWarehouse/CustomerWarehouse.Database.sql
+++ SQLBackupFolder/02112022_CustomerWarehouse/CustomerWarehouse.Database.sql
@@ -1,6 +1,6 @@
 ﻿USE [master]
 GO
-/****** Object:  Database [CustomerWarehouse]    Script Date: 02.11.2022 14:12:33 ******/
+/****** Object:  Database [CustomerWarehouse]    Script Date: 02.11.2022 14:17:17 ******/
 CREATE DATABASE [CustomerWarehouse]
  CONTAINMENT = NONE
  ON  PRIMARY 
--- SQLBackupFolder/02112022_CustomerWarehouse/dbo.factCustomer.Table.sql
+++ SQLBackupFolder/02112022_CustomerWarehouse/dbo.factCustomer.Table.sql
@@ -1,6 +1,6 @@
 ﻿USE [CustomerWarehouse]
 GO
-/****** Object:  Table [dbo].[factCustomer]    Script Date: 02.11.2022 14:12:33 ******/
+/****** Object:  Table [dbo].[factCustomer]    Script Date: 02.11.2022 14:17:18 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
