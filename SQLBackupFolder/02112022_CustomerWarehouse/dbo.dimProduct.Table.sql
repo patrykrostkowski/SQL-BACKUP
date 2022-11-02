c5ef2994eb31a058144dcd769d5d0584be82d8e6
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-02 09:08:57 +0100

    02.11.2022_CustomerWarehouse backup

---
+++ SQLBackupFolder/02112022_CustomerWarehouse/dbo.dimProduct.Table.sql
@@ -0,0 +1,16 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[dimProduct]    Script Date: 02.11.2022 09:08:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[dimProduct](
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dimProduct] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ SQLBackupFolder/02112022_CustomerWarehouse/dbo.dimSalesPerson.Table.sql
@@ -0,0 +1,16 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[dimSalesPerson]    Script Date: 02.11.2022 09:08:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[dimSalesPerson](
+	[SalesPersonID] [int] NOT NULL,
+	[SalesPersonName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dimSalesPerson] PRIMARY KEY CLUSTERED 
+(
+	[SalesPersonID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
