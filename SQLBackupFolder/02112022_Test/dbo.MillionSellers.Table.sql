6fd2c8bb0f4aa07d9a507583690de42a436d4dc2
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-02 12:58:22 +0100

    02.11.2022_Test backup

--- SQLBackupFolder/02112022_Test/dbo.MillionSellers.Table.sql
+++ SQLBackupFolder/02112022_Test/dbo.MillionSellers.Table.sql
@@ -1,6 +1,6 @@
 ﻿USE [Test]
 GO
-/****** Object:  Table [dbo].[MillionSellers]    Script Date: 02.11.2022 12:56:02 ******/
+/****** Object:  Table [dbo].[MillionSellers]    Script Date: 02.11.2022 12:58:20 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
