c747d4872ea736fe3626824dab2da0b848ea650a
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-02 14:37:33 +0100

    02.11.2022_Test backup

--- SQLBackupFolder/02112022_Test/dbo.Mentor.Table.sql
+++ SQLBackupFolder/02112022_Test/dbo.Mentor.Table.sql
@@ -1,6 +1,6 @@
 ﻿USE [Test]
 GO
-/****** Object:  Table [dbo].[Mentor]    Script Date: 02.11.2022 14:17:34 ******/
+/****** Object:  Table [dbo].[Mentor]    Script Date: 02.11.2022 14:37:31 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
