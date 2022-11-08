2c7dcae371320ced87690dc283efecf1c5c0248a
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-08 09:01:15 +0100

    08.11.2022_AdventureWorks2019 backup

---
+++ SQLBackupFolder/08112022_AdventureWorks2019/Production.Schema.sql
@@ -0,0 +1,7 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  Schema [Production]    Script Date: 08.11.2022 09:01:12 ******/
+CREATE SCHEMA [Production]
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to products, inventory, and manufacturing.' , @level0type=N'SCHEMA',@level0name=N'Production'
+GO
