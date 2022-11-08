a14a7211ab5019465ff77afc78a00352536c82af
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-08 08:48:05 +0100

    08.11.2022_Test backup

---
+++ SQLBackupFolder/08112022_Test/dbo.UKBuiltUpAreas.Table.sql
@@ -0,0 +1,13 @@
+﻿USE [Test]
+GO
+/****** Object:  Table [dbo].[UKBuiltUpAreas]    Script Date: 08.11.2022 08:48:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UKBuiltUpAreas](
+	[Rank] [int] NULL,
+	[AreaName] [varchar](255) NULL,
+	[Population] [bigint] NULL
+) ON [PRIMARY]
+GO
