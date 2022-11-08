c176608d5efd172f689effee75f6908baa78cbed
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-08 08:53:16 +0100

    no message

--- SQLBackupFolder/08112022_AdventureWorks2019/dbo.NameStyle.UserDefinedDataType.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  UserDefinedDataType [dbo].[NameStyle]    Script Date: 08.11.2022 08:51:08 ******/
-CREATE TYPE [dbo].[NameStyle] FROM [bit] NOT NULL
-GO
--- SQLBackupFolder/08112022_AdventureWorks2019/dbo.OrderNumber.UserDefinedDataType.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  UserDefinedDataType [dbo].[OrderNumber]    Script Date: 08.11.2022 08:51:08 ******/
-CREATE TYPE [dbo].[OrderNumber] FROM [nvarchar](25) NULL
-GO
