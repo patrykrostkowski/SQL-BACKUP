f8cf5e156e7cce3b5f875ab86eeabf58db4bb312
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-08 08:49:07 +0100

    no message

--- SQLBackupFolder/08112022_AdventureWorks2019/dbo.AccountNumber.UserDefinedDataType.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  UserDefinedDataType [dbo].[AccountNumber]    Script Date: 08.11.2022 08:47:40 ******/
-CREATE TYPE [dbo].[AccountNumber] FROM [nvarchar](15) NULL
-GO
