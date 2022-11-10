c659075a867a18b52ced0f7b879d0703c7afb4df
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-10 13:47:19 +0100

    10.11.2022_AdventureWorks2019 backup

---
+++ SQLBackupFolder/10112022_AdventureWorks2019/Production.Schema.sql
@@ -0,0 +1,9 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  Schema [Production]    Script Date: 10.11.2022 13:47:15 ******/
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'Production')
+EXEC sys.sp_executesql N'CREATE SCHEMA [Production]'
+GO
+IF NOT EXISTS (SELECT * FROM sys.fn_listextendedproperty(N'MS_Description' , N'SCHEMA',N'Production', NULL,NULL, NULL,NULL))
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to products, inventory, and manufacturing.' , @level0type=N'SCHEMA',@level0name=N'Production'
+GO
---
+++ SQLBackupFolder/10112022_AdventureWorks2019/Sales.Schema.sql
@@ -0,0 +1,9 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  Schema [Sales]    Script Date: 10.11.2022 13:47:15 ******/
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'Sales')
+EXEC sys.sp_executesql N'CREATE SCHEMA [Sales]'
+GO
+IF NOT EXISTS (SELECT * FROM sys.fn_listextendedproperty(N'MS_Description' , N'SCHEMA',N'Sales', NULL,NULL, NULL,NULL))
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to customers, sales orders, and sales territories.' , @level0type=N'SCHEMA',@level0name=N'Sales'
+GO
