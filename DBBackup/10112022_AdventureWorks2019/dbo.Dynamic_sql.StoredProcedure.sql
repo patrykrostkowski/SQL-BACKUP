e2f369c3298c2b4e791c810f682424f98962e901
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-10 14:20:33 +0100

    10.11.2022_AdventureWorks2019 backup

---
+++ DBBackup/10112022_AdventureWorks2019/dbo.Dynamic_sql.StoredProcedure.sql
@@ -0,0 +1,35 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  StoredProcedure [dbo].[Dynamic_sql]    Script Date: 10.11.2022 14:20:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Dynamic_sql]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[Dynamic_sql] AS' 
+END
+GO
+ALTER PROCEDURE [dbo].[Dynamic_sql] (@NameToSearch VARCHAR(10),@SearchPattern VARCHAR(30))
+AS
+BEGIN
+	DECLARE @DynamicSQL VARCHAR(MAX)
+	DECLARE @ColumnName VARCHAR(20)
+
+		IF LOWER(@NameToSearch) = 'first'
+			SET @ColumnName = 'FirstName'
+		IF LOWER(@NameToSearch) = 'middle'
+			SET @ColumnName = 'MiddleName'
+		IF LOWER(@NameToSearch) = 'last'
+			SET @ColumnName = 'LastName'	
+			
+	SET @DynamicSQL = 'SELECT *
+						FROM Person.Person pp
+						WHERE 
+							pp.' + @ColumnName
+	SET @DynamicSQL = @DynamicSQL + '	LIKE ''%' + @SearchPattern + '%'''
+
+EXEC (@DynamicSQL)
+	
+END
+GO
---
+++ DBBackup/10112022_AdventureWorks2019/dbo.Name.UserDefinedDataType.sql
@@ -0,0 +1,6 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  UserDefinedDataType [dbo].[Name]    Script Date: 10.11.2022 14:20:29 ******/
+IF NOT EXISTS (SELECT * FROM sys.types st JOIN sys.schemas ss ON st.schema_id = ss.schema_id WHERE st.name = N'Name' AND ss.name = N'dbo')
+CREATE TYPE [dbo].[Name] FROM [nvarchar](50) NULL
+GO
