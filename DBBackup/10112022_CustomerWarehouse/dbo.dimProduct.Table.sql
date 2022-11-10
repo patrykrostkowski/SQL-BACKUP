7ff3d8a301274fcdfaca00e582bf2a956902317c
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-10 14:34:57 +0100

    10.11.2022_CustomerWarehouse backup

---
+++ DBBackup/10112022_CustomerWarehouse/dbo.dimProduct.Table.sql
@@ -0,0 +1,19 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[dimProduct]    Script Date: 10.11.2022 14:34:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[dimProduct]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[dimProduct](
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dimProduct] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
