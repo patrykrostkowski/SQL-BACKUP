c8b6f28ab929726261bf0372379e98fd7f89843a
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-10 12:34:17 +0100

    10.11.2022_CustomerWarehouse backup

---
+++ SQLBackupFolder/10112022_CustomerWarehouse/dbo.dimCountry.Table.sql
@@ -0,0 +1,16 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[dimCountry]    Script Date: 10.11.2022 12:34:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[dimCountry](
+	[CountryID] [int] NOT NULL,
+	[CountryName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dimCountry] PRIMARY KEY CLUSTERED 
+(
+	[CountryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ SQLBackupFolder/10112022_CustomerWarehouse/dbo.factCustomer.Table.sql
@@ -0,0 +1,18 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[factCustomer]    Script Date: 10.11.2022 12:34:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[factCustomer](
+	[CustomerCode] [nvarchar](50) NULL,
+	[CustomerName] [nvarchar](50) NULL,
+	[CustomerAmount] [int] NULL,
+	[SalesDate] [nvarchar](50) NULL,
+	[CountryID] [int] NULL,
+	[StatesID] [int] NULL,
+	[ProductID] [int] NULL,
+	[SalesPersonID] [int] NULL
+) ON [PRIMARY]
+GO
