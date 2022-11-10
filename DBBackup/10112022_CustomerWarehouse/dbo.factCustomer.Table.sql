f2b810178a4d074273efec24d1cc85e8af8a6ceb
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-10 14:20:38 +0100

    10.11.2022_CustomerWarehouse backup

---
+++ DBBackup/10112022_CustomerWarehouse/dbo.factCustomer.Table.sql
@@ -0,0 +1,21 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[factCustomer]    Script Date: 10.11.2022 14:20:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[factCustomer]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[factCustomer](
+	[CustomerCode] [nvarchar](50) NULL,
+	[CustomerName] [nvarchar](50) NULL,
+	[CustomerAmount] [int] NULL,
+	[SalesDate] [nvarchar](50) NULL,
+	[CountryID] [int] NULL,
+	[StatesID] [int] NULL,
+	[ProductID] [int] NULL,
+	[SalesPersonID] [int] NULL
+) ON [PRIMARY]
+END
+GO
---
+++ DBBackup/10112022_CustomerWarehouse/dbo.factCustomerDest.Table.sql
@@ -0,0 +1,21 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[factCustomerDest]    Script Date: 10.11.2022 14:20:37 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[factCustomerDest]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[factCustomerDest](
+	[CustomerCode] [nvarchar](50) NULL,
+	[CustomerName] [nvarchar](50) NULL,
+	[CustomerAmount] [nvarchar](50) NULL,
+	[SalesDate] [nvarchar](50) NULL,
+	[CountryID] [int] NULL,
+	[StatesID] [int] NULL,
+	[ProductID] [int] NULL,
+	[SalesPersonID] [int] NULL
+) ON [PRIMARY]
+END
+GO
