da15ceb19f5539b264c3d86d98c2b3fafc755c72
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-12 09:00:56 +0100

    12.11.2022_AdventureWorks2019 backup

---
+++ DBBackup/12112022_AdventureWorks2019/AW2016FullTextCatalog.FullTextCatalog.sql
@@ -0,0 +1,6 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  FullTextCatalog [AW2016FullTextCatalog]    Script Date: 12.11.2022 09:00:52 ******/
+IF NOT EXISTS (SELECT * FROM sysfulltextcatalogs ftc WHERE ftc.name = N'AW2016FullTextCatalog')
+CREATE FULLTEXT CATALOG [AW2016FullTextCatalog] AS DEFAULT
+GO
---
+++ DBBackup/12112022_AdventureWorks2019/Purchasing.Schema.sql
@@ -0,0 +1,9 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  Schema [Purchasing]    Script Date: 12.11.2022 09:00:52 ******/
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'Purchasing')
+EXEC sys.sp_executesql N'CREATE SCHEMA [Purchasing]'
+GO
+IF NOT EXISTS (SELECT * FROM sys.fn_listextendedproperty(N'MS_Description' , N'SCHEMA',N'Purchasing', NULL,NULL, NULL,NULL))
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to vendors and purchase orders.' , @level0type=N'SCHEMA',@level0name=N'Purchasing'
+GO
