e6906d0392e99168372a073cb101a27d57e42e47
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-12 10:11:59 +0100

    12.11.2022_AdventureWorks2019 backup

---
+++ DBBackup/AdventureWorks2019/dbo.Flag.UserDefinedDataType.sql
@@ -0,0 +1,6 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  UserDefinedDataType [dbo].[Flag]    Script Date: 12.11.2022 10:11:55 ******/
+IF NOT EXISTS (SELECT * FROM sys.types st JOIN sys.schemas ss ON st.schema_id = ss.schema_id WHERE st.name = N'Flag' AND ss.name = N'dbo')
+CREATE TYPE [dbo].[Flag] FROM [bit] NOT NULL
+GO
---
+++ DBBackup/AdventureWorks2019/dbo.OrdersAboveThreshold.StoredProcedure.sql
@@ -0,0 +1,26 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  StoredProcedure [dbo].[OrdersAboveThreshold]    Script Date: 12.11.2022 10:11:56 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[OrdersAboveThreshold]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[OrdersAboveThreshold] AS' 
+END
+GO
+
+ALTER PROCEDURE [dbo].[OrdersAboveThreshold] 
+	(@Threshold INT, @StartYear INT, @EndYear INT)
+AS
+BEGIN
+	SELECT
+		SalesOrderID
+		, SUM(LineTotal) AS sum_linetotal
+	FROM Sales.SalesOrderDetail
+	WHERE YEAR(ModifiedDate) BETWEEN @StartYear AND @EndYear
+	GROUP BY SalesOrderID
+	HAVING SUM(LineTotal) > @Threshold
+END
+GO
