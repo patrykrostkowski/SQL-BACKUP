98388b5634ed5368cf0937e6a998d458aebe1af7
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-12 09:01:35 +0100

    12.11.2022_Test backup

---
+++ DBBackup/12112022_Test/dbo.FoodMusic.Table.sql
@@ -0,0 +1,16 @@
+﻿USE [Test]
+GO
+/****** Object:  Table [dbo].[FoodMusic]    Script Date: 12.11.2022 09:01:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FoodMusic]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[FoodMusic](
+	[Position] [int] NULL,
+	[Artist] [nvarchar](255) NULL,
+	[SongTitle] [nvarchar](255) NULL
+) ON [PRIMARY]
+END
+GO
---
+++ DBBackup/12112022_Test/dbo.tblBushTuckerItem.Table.sql
@@ -0,0 +1,19 @@
+﻿USE [Test]
+GO
+/****** Object:  Table [dbo].[tblBushTuckerItem]    Script Date: 12.11.2022 09:01:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblBushTuckerItem]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[tblBushTuckerItem](
+	[BushTuckerItem] [int] NOT NULL,
+	[BushTuckerName] [nvarchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[BushTuckerItem] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
