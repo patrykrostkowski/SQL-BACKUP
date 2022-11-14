f12bc71d8b1d05ab1773f58b3825063e62910082
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-14 12:07:01 +0100

    14.11.2022_CustomerWarehouse backup

---
+++ DBBackup/CustomerWarehouse/14.11.2022/dbo.factCustomerDest.Table.sql
@@ -0,0 +1,18 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[factCustomerDest]    Script Date: 14.11.2022 12:06:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[factCustomerDest](
+	[CustomerCode] [nvarchar](50) NULL,
+	[CustomerName] [nvarchar](50) NULL,
+	[CustomerAmount] [nvarchar](50) NULL,
+	[SalesDate] [nvarchar](50) NULL,
+	[CountryID] [int] NULL,
+	[StatesID] [int] NULL,
+	[ProductID] [int] NULL,
+	[SalesPersonID] [int] NULL
+) ON [PRIMARY]
+GO
