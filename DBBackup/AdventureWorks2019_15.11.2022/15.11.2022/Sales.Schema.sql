c41e1200be66d0ffc5f9b43fbd8911c19d4a34d2
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-15 16:52:40 +0100

    15.11.2022_AdventureWorks2019 backup

---
+++ DBBackup/AdventureWorks2019_15.11.2022/15.11.2022/Sales.Schema.sql
@@ -0,0 +1,7 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  Schema [Sales]    Script Date: 15.11.2022 16:52:36 ******/
+CREATE SCHEMA [Sales]
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to customers, sales orders, and sales territories.' , @level0type=N'SCHEMA',@level0name=N'Sales'
+GO
