43ba6ccde387c1766c5507efa2b691c7fcac7861
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-15 17:04:14 +0100

    15.11.2022_AdventureWorks2019 backup

---
+++ DBBackup/AdventureWorks2019_15112022/dbo.AccountNumber.UserDefinedDataType.sql
@@ -0,0 +1,5 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  UserDefinedDataType [dbo].[AccountNumber]    Script Date: 15.11.2022 17:04:11 ******/
+CREATE TYPE [dbo].[AccountNumber] FROM [nvarchar](15) NULL
+GO
---
+++ DBBackup/AdventureWorks2019_15112022/dbo.OrderNumber.UserDefinedDataType.sql
@@ -0,0 +1,5 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  UserDefinedDataType [dbo].[OrderNumber]    Script Date: 15.11.2022 17:04:11 ******/
+CREATE TYPE [dbo].[OrderNumber] FROM [nvarchar](25) NULL
+GO
