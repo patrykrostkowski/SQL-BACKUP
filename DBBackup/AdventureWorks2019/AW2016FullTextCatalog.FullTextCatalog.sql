1d8a405629046b26a4558f6d158f046fa81b5412
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-15 15:14:26 +0100

    no message

--- DBBackup/AdventureWorks2019/AW2016FullTextCatalog.FullTextCatalog.sql
+++
@@ -1,6 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  FullTextCatalog [AW2016FullTextCatalog]    Script Date: 14.11.2022 12:02:48 ******/
-IF NOT EXISTS (SELECT * FROM sysfulltextcatalogs ftc WHERE ftc.name = N'AW2016FullTextCatalog')
-CREATE FULLTEXT CATALOG [AW2016FullTextCatalog] AS DEFAULT
-GO
--- DBBackup/AdventureWorks2019/Person.Schema.sql
+++
@@ -1,9 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  Schema [Person]    Script Date: 14.11.2022 12:02:48 ******/
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'Person')
-EXEC sys.sp_executesql N'CREATE SCHEMA [Person]'
-GO
-IF NOT EXISTS (SELECT * FROM sys.fn_listextendedproperty(N'MS_Description' , N'SCHEMA',N'Person', NULL,NULL, NULL,NULL))
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to names and addresses of customers, vendors, and employees' , @level0type=N'SCHEMA',@level0name=N'Person'
-GO
