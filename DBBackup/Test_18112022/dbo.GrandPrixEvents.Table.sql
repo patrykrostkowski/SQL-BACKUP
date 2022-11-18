362acdec546050c44c4a4b0d04a260f3cd42d8dc
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-18 16:48:19 +0100

    18.11.2022_Test backup

---
+++ DBBackup/Test_18112022/dbo.GrandPrixEvents.Table.sql
@@ -0,0 +1,18 @@
+﻿USE [Test]
+GO
+/****** Object:  Table [dbo].[GrandPrixEvents]    Script Date: 18.11.2022 16:48:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[GrandPrixEvents](
+	[GrandPrixId] [int] IDENTITY(1,1) NOT NULL,
+	[Round] [int] NULL,
+	[VenueName] [nvarchar](255) NULL,
+	[Season] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[GrandPrixId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ DBBackup/Test_18112022/dbo.tblContestant.Table.sql
@@ -0,0 +1,13 @@
+﻿USE [Test]
+GO
+/****** Object:  Table [dbo].[tblContestant]    Script Date: 18.11.2022 16:48:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tblContestant](
+	[Contestant] [nvarchar](255) NULL,
+	[Position] [int] NULL,
+	[Mentor] [nvarchar](255) NULL
+) ON [PRIMARY]
+GO
