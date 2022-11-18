398414063c725d18d4c701994f9e21c455a13bab
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-18 15:00:35 +0100

    18.11.2022_AdventureWorks2019 backup

--- DBBackup/AdventureWorks2019_18112022/dbo.NameStyle.UserDefinedDataType.sql
+++
@@ -1,5 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  UserDefinedDataType [dbo].[NameStyle]    Script Date: 18.11.2022 14:56:16 ******/
-CREATE TYPE [dbo].[NameStyle] FROM [bit] NOT NULL
-GO
