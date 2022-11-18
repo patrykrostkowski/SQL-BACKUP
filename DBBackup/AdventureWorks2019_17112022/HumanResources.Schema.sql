8870b7dae9d2c5fc79df441b037981c3974b3d8d
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-18 15:33:35 +0100

    no message

--- DBBackup/AdventureWorks2019_17112022/HumanResources.Schema.sql
+++
@@ -1,7 +0,0 @@
-﻿USE [AdventureWorks2019]
-GO
-/****** Object:  Schema [HumanResources]    Script Date: 18.11.2022 14:15:02 ******/
-CREATE SCHEMA [HumanResources]
-GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to employees and departments.' , @level0type=N'SCHEMA',@level0name=N'HumanResources'
-GO
