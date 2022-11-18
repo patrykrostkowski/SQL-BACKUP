9f2906b8056a7ce8a27e07672fb9cfc375605684
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-18 14:56:19 +0100

    18.11.2022_AdventureWorks2019 backup

---
+++ DBBackup/AdventureWorks2019_18112022/dbo.OrdersAboveThreshold.StoredProcedure.sql
@@ -0,0 +1,21 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  StoredProcedure [dbo].[OrdersAboveThreshold]    Script Date: 18.11.2022 14:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[OrdersAboveThreshold] 
+	(@Threshold INT, @StartYear INT, @EndYear INT)
+AS
+BEGIN
+	SELECT
+		SalesOrderID
+		, SUM(LineTotal) AS sum_linetotal
+	FROM Sales.SalesOrderDetail
+	WHERE YEAR(ModifiedDate) BETWEEN @StartYear AND @EndYear
+	GROUP BY SalesOrderID
+	HAVING SUM(LineTotal) > @Threshold
+END
+GO
