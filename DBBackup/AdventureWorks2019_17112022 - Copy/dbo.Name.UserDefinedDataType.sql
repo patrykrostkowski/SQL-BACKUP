0360491614d326db5d7508e482f5cf9f4a31936f
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-18 16:26:14 +0100

    no message

---
+++ DBBackup/AdventureWorks2019_17112022 - Copy/dbo.Name.UserDefinedDataType.sql
@@ -0,0 +1,5 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  UserDefinedDataType [dbo].[Name]    Script Date: 18.11.2022 16:18:44 ******/
+CREATE TYPE [dbo].[Name] FROM [nvarchar](50) NULL
+GO
---
+++ DBBackup/CustomerWarehouse_17112022 - Copy (2)/dbo.factCustomerDest.Table.sql
@@ -0,0 +1,18 @@
+﻿USE [CustomerWarehouse]
+GO
+/****** Object:  Table [dbo].[factCustomerDest]    Script Date: 18.11.2022 16:19:07 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[factCustomerDest](
+	[CustomerCode] [nvarchar](50) NULL,
+	[CustomerName] [nvarchar](50) NULL,
+	[CustomerAmount] [nvarchar](50) NULL,
+	[SalesDate] [nvarchar](50) NULL,
+	[CountryID] [int] NULL,
+	[StatesID] [int] NULL,
+	[ProductID] [int] NULL,
+	[SalesPersonID] [int] NULL
+) ON [PRIMARY]
+GO
