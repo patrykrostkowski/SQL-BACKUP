43b490ffb4225623c8910516dba0088f6e05a2c5
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-20 14:06:30 +0100

    20.11.2022_AdventureWorks2019 backup

---
+++ DBBackup/AdventureWorks2019_20112022/Person.Schema.sql
@@ -0,0 +1,7 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  Schema [Person]    Script Date: 20.11.2022 14:06:26 ******/
+CREATE SCHEMA [Person]
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to names and addresses of customers, vendors, and employees' , @level0type=N'SCHEMA',@level0name=N'Person'
+GO
---
+++ DBBackup/AdventureWorks2019_20112022/dbo.NameStyle.UserDefinedDataType.sql
@@ -0,0 +1,5 @@
+﻿USE [AdventureWorks2019]
+GO
+/****** Object:  UserDefinedDataType [dbo].[NameStyle]    Script Date: 20.11.2022 14:06:26 ******/
+CREATE TYPE [dbo].[NameStyle] FROM [bit] NOT NULL
+GO
