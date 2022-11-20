cf083ae6f1bd914caf875565f80a9072acd1e5cf
patrykrostkowski <105629801+patrykrostkowski@users.noreply.github.com>
2022-11-20 14:06:56 +0100

    20.11.2022_Test backup

---
+++ DBBackup/Test_20112022/dbo.BigBangEpisodes.Table.sql
@@ -0,0 +1,14 @@
+﻿USE [Test]
+GO
+/****** Object:  Table [dbo].[BigBangEpisodes]    Script Date: 20.11.2022 14:06:54 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[BigBangEpisodes](
+	[SeasonNumber] [int] NULL,
+	[EpisodeNumber] [int] NULL,
+	[DateBroadcast] [date] NULL,
+	[Title] [varchar](255) NULL
+) ON [PRIMARY]
+GO
---
+++ DBBackup/Test_20112022/dbo.Periods.Table.sql
@@ -0,0 +1,11 @@
+﻿USE [Test]
+GO
+/****** Object:  Table [dbo].[Periods]    Script Date: 20.11.2022 14:06:54 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Periods](
+	[Period] [nvarchar](255) NULL
+) ON [PRIMARY]
+GO
